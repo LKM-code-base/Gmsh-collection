66b29ba984ed6792f56666ee8eebc458c7a626d4
sebglane <seb_glane@gmx.de>
2021-06-02 08:56:26 +0200

    Force pure XDMF output to avoid bug with HDF5

--- tests/test_all_meshes.py
+++ tests/test_all_meshes.py
@@ -10,10 +10,8 @@ def test_all_meshes():
     for file in geo_files:
         generate_xdmf_mesh(file)
         subprocess.run(["rm", "-f", file.replace(".geo", ".msh")], check=True)
-        subprocess.run(["rm", "-f", file.replace(".geo", ".h5")], check=True)
         subprocess.run(["rm", "-f", file.replace(".geo", ".xdmf")], check=True)
         facet_file = file[:file.index(".geo")] + "_facet_markers"
-        subprocess.run(["rm", "-f", facet_file + ".h5"], check=True)
         subprocess.run(["rm", "-f", facet_file + ".xdmf"], check=True)
 
 
